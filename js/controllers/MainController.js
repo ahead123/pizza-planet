bd58c245015e8f5b3ed18a374efc1c04e84e2aa3
Arthur Head <arthurallenhead@yahoo.com>
2015-04-20 22:01:22 +0000

    finished pizza menu angular app

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,7 +11,48 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 4.95
     }
   ];
-
+  
+  $scope.mains = [
+    {
+      name: 'Pizza Medditeraneo',
+      description: 'Fennel, sausage, ricotta, olive',
+      price: 10.95
+    },
+    {
+      name: 'Spaghetti Classico',
+      description: 'Angel hair pasta, olive oil, lemon, garlic',
+      price: 12.95
+    },
+    {
+      name: 'Sea Bass',
+      description: 'Blackened, rosemary, capers, lemon oil',
+      price: 15.95
+    }
+  ];
+  
+   $scope.extras = [
+    {
+      name: 'Breadsticks',
+      description: 'Old country butter, parmesan',
+      price: 1.95
+    },
+    {
+      name: 'Garlic Knots',
+      description: 'Special dough, garlic butter toss',
+      price: 2.95
+    },
+    {
+      name: 'Olives',
+      description: 'Italian vineyard green olives',
+      price: 3.95
+    }
+  ];
+  
 }]);
